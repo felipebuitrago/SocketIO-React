67745fdc74a9de33191d90bcf9fce8d5c4e45292
Felipe Buitrago <flipe282000@gmail.com>
2023-10-30 10:06:34 -0500

    readme

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from 'react';
 import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import { socket } from './socket';
 import './styles.css';
 
@@ -57,52 +56,49 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
-        <br />
-        <h3>Vite + React Socket Chat</h3>
+        <h3>React Socket Chat</h3>
       </div>
 
-        <div className="card">
+      <div className="card">
           
-          <p>{ `Connected: ${isConnected}` }</p>
+        <p>{ `Connected: ${isConnected}` }</p>
 
-
-          <button onClick={ connect }>Connect</button>
-          <button onClick={ disconnect }>Disconnect</button>
-
+        <button onClick={ connect }>Connect</button>
+        <button onClick={ disconnect }>Disconnect</button>
       </div>
 
-      <textarea 
-        readOnly
-        disabled
-        value={ receivedMessage }
-        name="receivedMessage" 
-        id="received-id" 
-        cols="40" 
-        rows="5"
-      />
-        
       <div className="card">
         
-          <input 
-            type="text" 
-            name="mensaje" 
-            id="message-id" 
-            placeholder='Message' 
-            value={message || ''}
-            onChange={(e) => {setMessage(e.target.value)}}
-          />
-          
-          <button onClick={ sendMessage } >Send</button>
+        <textarea 
+          className='non-resized'
+          readOnly
+          disabled
+          value={ receivedMessage }
+          name="receivedMessage" 
+          id="received-id" 
+          cols="33" 
+          rows="5"
+        />
+        
+        <br />
+        
+        <input 
+          type="text" 
+          name="mensaje" 
+          id="message-id" 
+          placeholder='Message' 
+          value={message || ''}
+          onChange={(e) => {setMessage(e.target.value)}}
+        />
+        
+        <button onClick={ sendMessage }>Send</button>
 
       </div>
     </>
   )
-}
+};
 
-export default App
+export default App;
